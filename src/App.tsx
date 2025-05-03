50bc93ab0cc9664cc560fc72a277185a18afa1ae
000-dir-wmi-0001 <momindy321@gmail.com>
2025-05-03 10:47:02 +0530

    changed in mm_001

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
     <>
       <div className='bg-gradient-to-tr from-pink-400 to-violet-500 w-full h-screen px-4 py-6 '>
       <div className='motion-opacity-in-0 motion-translate-y-in-100 motion-blur-in-md backdrop-blur-md bg-white/40 rounded-full p-10 shadow-lg shadow-gray-50 text-center'>
-        <span className='text-black text-9xl font-bold text-shadow-lg text-shadow-gray-50'>Al Momin</span>
+        <span className='text-black text-9xl font-bold text-shadow-lg text-shadow-gray-100'>Al Momin</span>
+        <span className='text-black text-9xl font-bold text-shadow-lg text-shadow-gray-50'>Al Ansar</span>
       </div>
       </div>
     </>
